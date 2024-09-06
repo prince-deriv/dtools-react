4b1d3be3644f830fd9ec2dc6c1f801271f3afda1
prince-deriv <prince@deriv.com>
2024-09-06 11:22:33 +0400

    Updates

--- inject.js
+++ inject.js
@@ -44,32 +44,28 @@
 // })();
 
 // Dtools Bookmark
-(function () {
-  function loadScript(src, callback) {
-    const script = document.createElement("script");
-    script.src = src;
-    script.type = "module";
-    script.onload = function () {
-      if (typeof callback === "function") {
-        callback();
-      }
-    };
-    script.onerror = function () {
-      console.error(`Failed to load the script: ${src}`);
-    };
-    document.head.appendChild(script);
-  }
+// javascript:(function() {
+//   function loadScript(src, callback) {
+//     const script = document.createElement('script');
+//     script.src = src;
+//     script.type = 'module';
+//     script.onload = function() {
+//       if (typeof callback === 'function') {
+//         callback();
+//       }
+//     };
+//     script.onerror = function() {
+//       console.error(`Failed to load the script: ${src}`);
+//     };
+//     document.head.appendChild(script);
+//   }
 
-  loadScript(
-    "https://prince-deriv.github.io/dtools-react/inject.js",
-    function () {
-      // Call the function from the loaded script
-      if (typeof loadDToolsProduction === "function") {
-        loadDToolsProduction();
-      }
-    }
-  );
-})();
+//   loadScript('https://prince-deriv.github.io/dtools-react/inject.js', function() {
+//     if (typeof loadDToolsProduction === 'function') {
+//       loadDToolsProduction();
+//     }
+//   });
+// })();
 
 const loadDToolsProduction = () => {
   if (document.getElementById("dtools-container")) return;
@@ -131,6 +127,26 @@ const loadDToolsProduction = () => {
         console.error("Error fetching manifest:", error);
       });
   })();
+};
+
+const loadDToolsLocal = () => {
+  if (document.getElementById("dtools-container-local")) return;
+  const container = document.createElement("div");
+  const iframe = document.createElement("iframe");
+  iframe.src = "http://localhost:8888/dtools-react";
+  container.id = "dtools-container-local";
+  container.style.position = "fixed";
+  container.style.bottom = "0";
+  container.style.right = "10px";
+  container.style.width = "350px";
+  container.style.height = "500px";
+  container.style.backgroundColor = "white";
+  container.style.boxShadow = "#000000 0px 0px 10px";
+  container.style.borderRadius = "10px 10px 0px 0px";
+  container.style.zIndex = "999999";
+  container.style.overflow = "hidden";
+  container.appendChild(iframe);
+  document.body.appendChild(container);
 };
 
 // // Local
